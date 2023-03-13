0019e3cac8835d06e0d34b0e292cf1652d13aa34
DEVBIG <olenadev7@gmail.com>
2023-03-13 12:20:12 +0200

    fix section1

--- script/section1-script.js
+++ script/section1-script.js
@@ -1,13 +1,21 @@
 $(window).scroll(function () {
     var scroll = $(window).scrollTop();
     if ((75 + scroll / 60) >= 100) {
-        $(".arc-img").css({
-            backgroundSize: "100%",
-        });
+        $('.img-container').css({
+            width: "100%",
+            height: '100%'
+        })
+        // $(".arc-img").css({
+        //     backgroundSize: "100%",
+        // });
     } else {
-        $(".arc-img").css({
-            backgroundSize: (75 + scroll / 60) + "%",
-        });
+        $('.img-container').css({
+            width: (75 + scroll / 60) + "%",
+            height: (75 + scroll / 60) + "%"
+        })
+        // $(".arc-img").css({
+        //     backgroundSize: (75 + scroll / 60) + "%",
+        // });
     }
     if (($(window).scrollTop() >= $('.section2').offset().top)) {
         $('header').removeClass('hide')
@@ -20,39 +28,65 @@ $(window).scroll(function () {
 var scroll = 0;
 var conponent = document.querySelector('.section1');
 var conponentBG = document.querySelector('.arc-img');
+var conponentImg1 = document.querySelector('.img1'),
+    conponentImg2 = document.querySelector('.img2'),
+    conponentImg3 = document.querySelector('.img3'),
+    conponentImg4 = document.querySelector('.img4'),
+    conponentImg5 = document.querySelector('.img5'),
+    conponentImg6 = document.querySelector('.img6'),
+    conponentImg7 = document.querySelector('.img7');
 var windowHeight = 230;
 document.addEventListener('scroll', () => {
     scroll = window.pageYOffset;
     // console.log(scroll)
     if (scroll >= 0 && scroll < windowHeight) {
         //to remove all classes sauf .container
-        conponentBG.setAttribute('class', 'arc-img');
-        conponentBG.classList.add('img1');
+        conponentImg2.setAttribute('class', 'img2');
+
+        conponentImg1.setAttribute('class', 'img1');
+        conponentImg1.classList.add('show');
     } else if (scroll >= windowHeight && scroll < 2 * windowHeight) {
         //to remove all classes sauf .container
-        conponentBG.setAttribute('class', 'arc-img');
-        conponentBG.classList.add('img2');
+        conponentImg1.setAttribute('class', 'img1');
+        conponentImg3.setAttribute('class', 'img3');
+
+        conponentImg2.setAttribute('class', 'img2');
+        conponentImg2.classList.add('show');
 
     } else if (scroll >= 2 * windowHeight && scroll < 3 * windowHeight) {
         //to remove all classes sauf .container
-        conponentBG.setAttribute('class', 'arc-img');
-        conponentBG.classList.add('img3');
+        conponentImg2.setAttribute('class', 'img2');
+        conponentImg4.setAttribute('class', 'img4');
+
+        conponentImg3.setAttribute('class', 'img3');
+        conponentImg3.classList.add('show');
     } else if (scroll >= 3 * windowHeight && scroll < 4 * windowHeight) {
         //to remove all classes sauf .container
-        conponentBG.setAttribute('class', 'arc-img');
-        conponentBG.classList.add('img4');
+        conponentImg3.setAttribute('class', 'img3');
+        conponentImg5.setAttribute('class', 'img5');
+
+        conponentImg4.setAttribute('class', 'img4');
+        conponentImg4.classList.add('show');
     } else if (scroll >= 4 * windowHeight && scroll < 5 * windowHeight) {
         //to remove all classes sauf .container
-        conponentBG.setAttribute('class', 'arc-img');
-        conponentBG.classList.add('img5');
+        conponentImg4.setAttribute('class', 'img4');
+        conponentImg6.setAttribute('class', 'img6');
+
+        conponentImg5.setAttribute('class', 'img5');
+        conponentImg5.classList.add('show');
     } else if (scroll >= 5 * windowHeight && scroll < 6 * windowHeight) {
         //to remove all classes sauf .container
-        conponentBG.setAttribute('class', 'arc-img');
-        conponentBG.classList.add('img6');
+        conponentImg5.setAttribute('class', 'img5');
+        conponentImg7.setAttribute('class', 'img7');
+
+        conponentImg6.setAttribute('class', 'img6');
+        conponentImg6.classList.add('show');
     } else if (scroll >= 6 * windowHeight && scroll < 7 * windowHeight) {
         //to remove all classes sauf .container
-        conponentBG.setAttribute('class', 'arc-img');
-        conponentBG.classList.add('img7');
+        conponentImg6.setAttribute('class', 'img6');
+
+        conponentImg7.setAttribute('class', 'img7');
+        conponentImg7.classList.add('show');
     }
 })
 
