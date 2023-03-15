db71221d1b392fe55968f989229097c491dfa6c4
DEVBIG <olenadev7@gmail.com>
2023-03-15 15:28:01 +0200

    fix

--- script/section3-script.js
+++ script/section3-script.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
         item3 = document.querySelector('.image-slide-item3');
     var w = $('.second-slider').height() / 5;
     var videoBG = document.querySelector('#video-background')
+    var pictureBG = document.querySelector('picture')
 
 
     $(window).scroll(function () {
@@ -16,22 +17,22 @@ $(document).ready(function () {
             section2.setAttribute('class', 'section2 hide');
             videoBG.style.width = '100%';
             videoBG.style.height = '100%';
-            // section3.setAttribute('class', 'section section3');
-            // section3.classList.add('active');
+            pictureBG.style.width = '100%';
+            pictureBG.style.height = '100%';
+           
         } else {
             section2.setAttribute('class', 'section2 show');
             videoBG.style.width = '105%';
             videoBG.style.height = '105%';
+            pictureBG.style.width = '105%';
+            pictureBG.style.height = '105%';
         }
-        // if ($(window).scrollTop() <= ($('.second-slider').offset().top + (2 * w)))  
-        
+       
         if (($(window).scrollTop() >= $('.second-slider').offset().top) &&
             ($(window).scrollTop() <= ($('.second-slider').offset().top + (1.5 * w)))) {
             section3.setAttribute('class', 'section section3');
             section3.classList.add('active');
             section4.setAttribute('class', 'section section4');
-
-            // section2.setAttribute('class', 'section2 hide');
 
         } else if ($(window).scrollTop() >= ($('.second-slider').offset().top + (1.5 * w)) &&
             ($(window).scrollTop() <= ($('.second-slider').offset().top + (2.5 * w)))) {
@@ -40,8 +41,7 @@ $(document).ready(function () {
             section3.classList.add('inActive')
             section4.setAttribute('class', 'section section4');
             section4.classList.add('active')
-            // section2.setAttribute('class', 'section2 hide');
-
+           
             item1.setAttribute('class', 'image-slide-item image-slide-item1');
             item1.classList.add('active')
             item2.setAttribute('class', 'image-slide-item image-slide-item2');
@@ -59,7 +59,6 @@ $(document).ready(function () {
             item1.setAttribute('class', 'image-slide-item image-slide-item1');
             item1.classList.add('inActive')
             item3.setAttribute('class', 'image-slide-item image-slide-item3');
-            // section2.setAttribute('class', 'section2 hide');
 
         } else if ($(window).scrollTop() >= ($('.second-slider').offset().top + (3.3 * w)) &&
             ($(window).scrollTop() <= ($('.second-slider').offset().top + (4.5 * w)))) {
